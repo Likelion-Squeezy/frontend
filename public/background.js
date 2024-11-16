d4c9e41acae9928d700bd99f194234607b9f51f7
SeHyun <rla6170@naver.com>
2024-11-16 15:59:07 +0900

    feat : api profile

--- public/background.js
+++ public/background.js
@@ -94,6 +94,29 @@ function extractHtml() {
 
 // 팝업의 버튼이 눌렸을 때의 preview.jsx/onSqueeze()의 메시지를 수신하기 위한 이벤트 리스너 등록
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "profile") {
+    (async function () {
+      try {
+        await fetch("http://13.124.143.64/api/users/profile/", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "751aae735c54cfed0965670c717acda12e5a2711",
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            sendResponse({ success: true, response: data });
+          })
+          .catch((error) => {
+            console.error("Error sending message:", error);
+            sendResponse({ success: false, error: error.message });
+          });
+      } catch (error) {
+        console.error("Error sending message:", error);
+      }
+    })();
+  }
   if (request.action === "preview") {
     (async function () {
       try {
@@ -263,19 +286,19 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 function previewTab(callback) {
   const previews = {
-    tabs : [],
+    tabs: [],
     capturedImage: "",
   };
   chrome.tabs.query({}, (tabs) => {
     tabs.forEach((tab, index) => {
       if (index > 0 && index != tab.index) return; // 현재 탭만 허용
       previews.tabs.push({
-        title : tab.title,
-        url : tab.url,
-        favicon : tab.favIconUrl,
-      })
+        title: tab.title,
+        url: tab.url,
+        favicon: tab.favIconUrl,
+      });
     });
-    
+
     chrome.tabs.captureVisibleTab(null, { format: "png" }, (dataUrl) => {
       if (dataUrl) {
         previews.capturedImage = dataUrl;
--- src/components/squeezy/SqueezyHistory.jsx
+++ src/components/squeezy/SqueezyHistory.jsx
@@ -1,7 +1,26 @@
 import styled from "styled-components";
 import { SqueezyList } from "./SqueezyList";
+import { useEffect } from "react";
 
 export const SqueezyHistroy = () => {
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        await new Promise((resolve, reject) => {
+          chrome.runtime.sendMessage({ action: "profile" }, (response) => {
+            resolve(response);
+          })
+        }).then((response) => {
+          console.log(response);
+        }).catch(error => {
+          console.error("Error sending message:", error);
+        });
+      } catch(error) {
+        console.error("Error sending message:", error);
+      }
+    };
+    fetchProfile();
+  }, [])
   return (
     <Container>
       <History>{`0 / 50 history`}</History>
