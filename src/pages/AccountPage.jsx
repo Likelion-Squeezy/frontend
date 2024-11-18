f2a76b833bf7ce4a3c75abd2019ae32418703419
SeHyun <rla6170@naver.com>
2024-11-19 02:26:43 +0900

    feat : account 처리

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,13 +1,22 @@
 import styled from "styled-components";
+import Logo from "@assets/icon/icon-logo--img.svg?react";
 
 export const AccountPage = () => {
   return (
     <Container>
-      <h1>Account Page</h1>
+      <h1>Account Settings page is under construction</h1>
+      <div style={{display : "flex", justifyContent : "center", marginTop : "50px"}}>
+        <Logo width={150} height={150}/>
+      </div>
     </Container>
   );
 };
 
 const Container = styled.div`
-  // CSS  
+  padding: 9px 14px;// CSS  
+  & > h1 {
+    ${(props) => props.theme.typography.h1};
+    color : rgba(0, 0, 0, 0.77);
+    font-weight: 700;
+  }
 `;
